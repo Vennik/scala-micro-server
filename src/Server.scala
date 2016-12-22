5e92a8b0b1c9e47735a8d2a3f90e36d1119e6d38
René Vennik <renevennik@gmail.com>
2016-12-22 19:22:27 +0100

    Inline Handler

--- src/Server.scala
+++ src/Server.scala
@@ -9,30 +9,29 @@ import scala.util.Try
   * @version 1.0
   * @since 22-12-2016
   */
-class Server(routes: String => Any, val port: Int = 80) {
+class Server(val routes: String => Any, val port: Int = 80) {
 
   val server: HttpServer = HttpServer.create(new InetSocketAddress(port), 0)
-  val handler = new Handler(routes)
-  server.createContext("/", handler)
+  server.createContext("/", new Handler())
   server.setExecutor(null)
   server.start()
 
-}
+  class Handler extends HttpHandler {
 
-class Handler(val routes: String => Any) extends HttpHandler {
-
-  def handle(t: HttpExchange): Unit = {
-    Try(routes(t.getRequestURI.getPath).toString).toOption match {
-      case Some(response) => handleResponse(t, 200, response)
-      case _ => handleResponse(t, 404, "404 - Not Found")
+    def handle(t: HttpExchange): Unit = {
+      Try(routes(t.getRequestURI.getPath).toString).toOption match {
+        case Some(response) => handleResponse(t, 200, response)
+        case _ => handleResponse(t, 404, "404 - Not Found")
+      }
     }
-  }
 
-  def handleResponse(t: HttpExchange, rCode: Int, response: String): Unit = {
-    t.sendResponseHeaders(rCode, response.length())
-    val os = t.getResponseBody
-    os.write(response.getBytes)
-    os.close()
+    def handleResponse(t: HttpExchange, rCode: Int, response: String): Unit = {
+      t.sendResponseHeaders(rCode, response.length())
+      val os = t.getResponseBody
+      os.write(response.getBytes)
+      os.close()
+    }
+
   }
 
 }
